816093347c893d900ab4adfd0ab6e569a465768b
adolfo zamora <adolfozamora1005@gmail.com>
2023-01-18 16:27:21 -0500

    update 18 de enero

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,12 +1,13 @@
 const {Sequelize} = require('sequelize');
+require("dotenv").config()
 //creamos una instancia con parametros de configuracion de nustra base de datos
 //un objeto de configuracion, con las credenciales de mi base de datos
 const db = new Sequelize({
-    database:"todoapp",
-    username:"postgres",
-    host:"localhost",//127.0.0.1 es el local host
-    port:"5432",
-    password:"ruta",//pones tu contraseña
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    host: process.env.DB_HOST,//127.0.0.1 es el local host
+    port: process.env.DB_PORT,
+    password: process.env.DB_PASSWORD,//pones tu contraseña
     dialect:"postgres",//la base de datos que estamos utilizando 
     logging:false,
     // dialectOptions: { ssl: { require: true, rejectUnauthorized: false}},
